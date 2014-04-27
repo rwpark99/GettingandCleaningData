55efdc47880c324251502d1943203a866f2849a2
Rae Woong Park <rwpark99@gmail.com>
2014-04-26 21:13:31 -0400

    v10.5

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,11 @@
 #########################################################
 #03 week 1 assignment
-# April 10. 2014, Rae Woong Park
+# April 26. 2014, Rae Woong Park
 #########################################################
 
 library(data.table)
-
+#setwd("C:/Users/Administrator/Documents/GitHub/GettingandCleaningData/")
+#setwd("C:/Users/Administrator/Documents/data/UCI HAR Dataset/")
 # 1. Merges the training and the test sets to create one data set.
 # & 3. Uses descriptive activity names to name the activities in the data set
 # $ 4. Appropriately labels the data set with descriptive activity names.
@@ -22,7 +23,9 @@ setnames(trainYData, "V2", "activity")
 setnames(testYData, "V2", "activity")
 
 col.names = read.table("features.TXT")
-col.namesModified <- gsub("[()-]","_", col.names$V2) # replace "(", ")", "-" into "_"
+col.namesModified <- gsub("[()]","_", col.names$V2) # replace "(", ")", "-" into "_"
+#col.namesModified <- gsub("-","", col.names$V2) # replace "(", ")", "-" into "_"
+
 
 trainSubjectData  <- read.table("./train/subject_train.txt", col.names="subject", sep="", header=FALSE)
 trainXData         <- read.table("./train/X_train.txt", sep="", col.names=col.namesModified, header=FALSE)
@@ -39,15 +42,13 @@ mergedData <-rbind(trainData, testData)
 write.table(mergedData, file = "mergedData.txt", append = FALSE, quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-meanStdData <- mergedData[, grep("subject|activity|mean__|std__{1}", colnames(mergedData), value=TRUE, ignore.case = FALSE)]
-write.table(meanStdData, file = "mergedMeanStrData.txt", append = FALSE, quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+meanStdData <- data.table(mergedData[, grep("subject|activity|mean__|std__", colnames(mergedData), value=TRUE, ignore.case = FALSE)])
 
 # 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
-summaryData = fread("mergedData.txt",  sep="auto", header=TRUE)
-setkey(summaryData, subject, activity)
-summaryDataWrite <- summaryData[, lapply(.SD, mean()), by="subject,activity"]
+setkey(meanStdData, subject, activity)
+summaryDataWrite <- meanStdData[, lapply(.SD, mean()), by="subject,activity"]
 write.table(summaryDataWrite, file="tidyData.txt", sep="\t", row.names=FALSE, col.names=TRUE)
 
 #########################################################
-# End of Assignment Week 1
+# End of Assignment
 #########################################################
